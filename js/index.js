a767602f1972a90b2d5efcb8119824c92f1a0f1f
Aron Bergman <bathingrad@gmail.com>
2017-10-26 12:55:32 +0200

    Parallax is working!
    Image is changed from tag to background, with background-position:
    fixed.
    The animation isn't laggy anymore!
    
    Note: Check issue #1 for more info about what might need to be changed
    from this commit

---
+++ js/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $("#hero").parallax(0.15);
+});
--- js/parallax.js
+++ js/parallax.js
@@ -1,12 +1,42 @@
+/*
 $(function() {
   if (!Modernizr.touchevents) {
     $(window).scroll(function() {
       requestAnimationFrame(parallax);
     });
   }
+*/
 
-  function parallax() {
-    var scrollTop = $(window).scrollTop();
-     $("#hero img").css("top", Math.round(scrollTop * 0.2) + "px");
+$(function() {
+  $.fn.parallax = function(speed) {
+    if ($(this).length > 1) {
+      $(this).each(function() {
+        $(this).parallax();
+      });
+    } else {
+      if (arguments.length < 1 || speed === null) {
+        speed = 0.15;
+      }
+
+      var element = $(this);
+      var height = element.height();
+      var top = element.offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+
+      $(window).resize(function() {
+        windowHeight = $(window).height();
+      });
+
+      // if scroll > (height + top) then the element is above the viewport
+      // if (scroll + windowHeight) < top then the element is below the viewport
+
+      $(window).scroll(function() {
+        scroll = $(window).scrollTop();
+        if (!(scroll > (height + top)) && !((scroll + windowHeight) < top)) {
+          element.css("transform", "translateY(" + Math.round((top - scroll) * speed) + "px)");
+        }
+      });
+    }
   }
 });
